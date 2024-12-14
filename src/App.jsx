bd524120fbcc803d0d59ac6020abf21bae5c426f
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2024-12-14 08:26:33 +0530

    removed css

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ function App() {
   return (
     <>
 
-    <ProductCard name="Laptop" src="https://picsum.photos/id/237/200/300" price="$99.99"></ProductCard>
+    <ProductCard name="Laptop" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" price="$99.99" />
 
-    <ProductCard name="Iphone" src="https://picsum.photos/id/237/200/300" price="$499"></ProductCard>
+    <ProductCard name="Iphone" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" price="$499" />
 
-    <ProductCard name="Samsung Galaxy" src="https://picsum.photos/id/237/200/300" price="$399"></ProductCard>
+    <ProductCard name="Samsung Galaxy" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" price="$399" />
 
-    <ProductCard name="Ipad" src="https://picsum.photos/id/237/200/300" price="$299"></ProductCard>
+    <ProductCard name="Ipad" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" price="$299" />
 
-    <UserData></UserData>
+    <UserData />
 
     </>
   )
