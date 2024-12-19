f8db9e5390e58326c0092bc73ad62c3f3c867bc7
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2024-12-20 00:12:45 +0530

    Started making the AdminHomePage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
 import SignupPage from './pages/signupPage'
+import AdminHomePage from './pages/adminHomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,8 +18,14 @@ function App() {
     <BrowserRouter>
     <Routes path="/*">
         <Route path="/" element={<HomePage />} />
+
         <Route path="/login" element={<LoginPage />} />
+
         <Route path="/signup" element={<SignupPage />} />
+
+        <Route path="/admin" element={<AdminHomePage />} />
+
+        <Route path="/*" element={<HomePage />} />
     </Routes>
     </BrowserRouter>
 
