d054bdfe2e95766c9e64cd398549604972c35bf5
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2024-12-20 01:40:11 +0530

    create userData file

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -2,9 +2,11 @@ export default function UserData() {
 
     return (
         <>
-        <img src="https://www.w3schools.com/howto/img_avatar.png"/>
-        <span>Kasun Sagara</span>
-        <span>Online</span>
+        <img src="https://www.w3schools.com/howto/img_avatar.png" className="w-[200px] h-[200px] rounded-full"/>
+
+        <span className="text-blue-600">Kasun Sagara</span>
+
+        <span className="text-green-700">Online</span>
         </>
     )
 
