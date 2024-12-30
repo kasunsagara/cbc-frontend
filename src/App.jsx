61ea1c7caeb74df2be9f0dbe72af8c541ebb1986
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2024-12-30 22:23:03 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import ProductCard from './components/productCard'
 import UserData from './components/userData'
-import Testing from './components/testing'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
@@ -18,15 +17,13 @@ function App() {
     <BrowserRouter>
     <Toaster position="top-right"/>
     <Routes path="/*">
-        <Route path="/" element={<HomePage />} />
+        <Route path="/*" element={<HomePage />} />
 
         <Route path="/login" element={<LoginPage />} />
 
         <Route path="/signup" element={<SignupPage />} />
 
         <Route path="/admin/*" element={<AdminHomePage />} />
-
-        <Route path="/*" element={<HomePage />} />
     </Routes>
     </BrowserRouter>
     </div>
