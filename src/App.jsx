f4cd29506842c843d0175b53139155d6b823e4e4
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-01-11 22:47:19 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import ProductCard from './components/productCard'
-import UserData from './components/userData'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
@@ -13,7 +11,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="">
+    <div className="bg-primary">
     <BrowserRouter>
     <Toaster position="top-right"/>
     <Routes path="/*">
