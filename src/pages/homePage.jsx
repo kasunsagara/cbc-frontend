fac9a0f1574cf59bf41c73580f4a47d50add60c8
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-02-09 16:40:08 +0530

    small change

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Header from '../components/header';
 import LoginPage from './loginPage';
 import ProductOverview from './home/productOverview';
+import ProductPage from './home/product';
+import Cart from './home/cart';
 
 export default function HomePage() {
   return (
@@ -10,7 +12,9 @@ export default function HomePage() {
       <div className='w-full h-[calc(100vh-100px)] '>
         <Routes path="/*">
           <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/products" element={<ProductPage/>} />
           <Route path="/login" element={<LoginPage/>} />
+          <Route path="/cart" element={<Cart/>}/>
           <Route path="/productInfo/:id" element={<ProductOverview/>} />
         </Routes>  
       </div>    
