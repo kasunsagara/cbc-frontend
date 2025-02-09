b2af7c8d424bcafd1e6dc96e45c810d43ce28cf4
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-02-09 16:20:40 +0530

    create productCard components

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,14 +1,24 @@
-export default function ProductCard(props) {
+import { Link } from "react-router-dom"
+
+export default function ProductCard(props){
 
     console.log(props)
-
+  
     return (
-        <div>
-            <h1>{props.name}</h1>
-            <img src={props.src}></img>
-            <h2>Price: {props.price}</h2>
-            <button>Add to cart</button>
+        <Link  to={`/productInfo/${props.product.productId}`}>
+      <div className="w-[300px] h-[450px] m-[70px]  rounded-xl shadow-lg shadow-gray-500 hover:shadow-primary  hover:border-[3px] overflow-hidden flex flex-col">
+        <img src={props.product.images[0]} className="h-[60%] w-full object-cover"/>
+        <div className=" max-h-[40%] h-[35%] p-4 flex flex-col justify-between">
+        <h1 className="text-3xl font-bold text-accent text-center">{props.product.productName}</h1>
+        <h2 className="text-lg text-gray-500 text-center">{props.product.productId}</h2>
+        <p className="text-left text-xl font-semibold">LKR. {props.product.lastPrice.toFixed(2)}</p>
+        {
+          (props.product.lastPrice<props.product.price)&&
+          <p className="text-left text-xl text-gray-500 font-semibold line-through ">LKR. {props.product.price.toFixed(2)}</p>
+        }
+        
         </div>
-    )
-
+      </div>
+    </Link>
+  )
 }
