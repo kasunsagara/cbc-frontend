a6ef0fc25a5d3c43f38ed1fd3d6bbd44f1f7d099
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-02-09 16:42:25 +0530

    import signupPage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from '../components/header';
 import LoginPage from './loginPage';
+import SignupPage from './signupPage';
 import ProductOverview from './home/productOverview';
 import ProductPage from './home/product';
 import Cart from './home/cart';
@@ -14,6 +15,7 @@ export default function HomePage() {
           <Route path="/" element={<h1>Home Page</h1>} />
           <Route path="/products" element={<ProductPage/>} />
           <Route path="/login" element={<LoginPage/>} />
+          <Route path="/signup" element={<SignupPage/>} />
           <Route path="/cart" element={<Cart/>}/>
           <Route path="/productInfo/:id" element={<ProductOverview/>} />
         </Routes>  
