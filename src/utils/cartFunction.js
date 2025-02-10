94450b95633e2e2702a49c94a2b9e832c2df92b3
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-02-10 22:17:18 +0530

    Made some changes to the cartFunction

--- src/utils/cartFunction.js
+++ src/utils/cartFunction.js
@@ -1,36 +1,52 @@
 export function loadCart() {
-    const cart = localStorage.getItem("cart");
-    if(cart!=null){
-      return JSON.parse(cart)
-    }else{
-      return []
-    }
+  const cart = localStorage.getItem("cart");
+  if(cart!=null){
+    return JSON.parse(cart)
+  }else{
+    return []
   }
-  export function addToCart(productId, qty){
-    const cart = loadCart()
-    const index = cart.findIndex(
-      (item)=>{
-        return item.productId==productId
-      }
+}
+
+export function addToCart(productId, qty){
+  const cart = loadCart()
+
+  const index = cart.findIndex(
+    (item)=>{
+      return item.productId==productId
+    }
+  )
+  console.log(index)
+  if(index==-1){
+    cart.push(
+      {productId, qty}
     )
-    console.log(index)
-    if(index==-1){
-      cart.push(
-        {productId, qty}
-      )
+  }else{
+
+    const newQty = cart[index].qty + qty
+    if(newQty<=0){
+      cart.splice(index,1)
     }else{
-      const newQty = cart[index].qty + qty
-      if(newQty<=0){
-        cart.splice(index,1)
-      }else{
-        cart[index].qty = newQty
-      }
+      cart[index].qty = newQty
     }
-    saveCart(cart)
   }
-  export function saveCart(cart){
-    localStorage.setItem("cart",JSON.stringify(cart))
+  saveCart(cart)
+}
+
+export function saveCart(cart){
+  localStorage.setItem("cart",JSON.stringify(cart))
+}
+
+export function clearCart(){
+  localStorage.removeItem("cart")
+}
+export function deleteItem(productId){
+  const cart = loadCart()
+  const index = cart.findIndex(
+    (item)=>{
+      return item.productId==productId
+    }
+  )
+  if(index!=-1){
+    cart.splice(index,1)
   }
-  export function clearCart(){
-    localStorage.removeItem("cart")
-  }
+}
